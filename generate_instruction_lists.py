2c6303f8174e04dda4b1aeef6758c4761a55812b
Ramses van Zon <rzon@scinet.utoronto.ca>
2019-11-12 12:34:45 -0500

    Simplifies instruction list generators, generating only one pdf

--- generate_instruction_lists.py
+++ generate_instruction_lists.py
@@ -1,5 +1,41 @@
 #!/usr/bin/env python3
+
 import random,os,time
+
+# Parameters
+
+participants = 16  # maximum number of participants
+bias = 2           # twice as likely to go forward than back.
+maxsteps = 48      # maximum number of steps your willing to take
+tiles = 10         # desired number of tiles: keep drawing instructions until the activity finished in 48 steps.
+
+# Find instructions that finish
+
+minmaxpos = 0
+while minmaxpos < tiles:
+    print ("************* DRAWING INSTRUCTIONS ****************")
+    instructions = [[] for i in range(participants)]
+    minmaxpos = 10000
+    for n in range(participants):
+        pos = 1
+        maxpos = 1
+        for i in range(maxsteps):
+            if random.randint(1, 1+bias)==1:
+                if pos > 1:
+                    pos = pos -1
+                    instructions[n].append("Go Back")
+                else:
+                    instructions[n].append("Stay")
+            else:
+                pos = pos + 1
+                instructions[n].append("Go Forward")
+            maxpos = max(maxpos, pos)    
+        minmaxpos = min(minmaxpos, maxpos)
+
+
+# Write instructions that finish
+        
+print ("************** GENERATING PDF INSTRUCTIONS *******************")
 
 latexstart = """\\documentclass[10pt]{article}
 \\usepackage{fullpage}
@@ -11,52 +47,31 @@ latexstart = """\\documentclass[10pt]{article}
 \\noindent
 \\Large
 \\begin{enumerate}
-\\itemsep0pt"""
+\\itemsep18pt"""
+
+latexsep = """\\end{enumerate}
+\\newpage
+\\begin{enumerate}
+\\itemsep18pt"""
 
 latexfinish = """\\end{enumerate}
 \\end{multicols}
 \\end{document}"""
 
-participants = 16  # maximum number of participants
-bias = 2           # twice as likely to go forward than back.
-maxsteps = 48      # maximum number of steps your willing to take
-tiles = 10         # desired number of tiles: keep drawing instructions until the activity finished in 48 steps.
+filenamebase = "randomall"
+filename = filenamebase + ".tex"
+with open(filename,"w") as f:
+    print (latexstart, file=f)
+    for n in range(participants):
+        for i in range(maxsteps):
+            print ("\\item\\fbox{%s}"%instructions[n][i], file=f)
+        if n < participants-1:
+            print (latexsep, file=f)   
+    print (latexfinish, file=f)
+os.system("pdflatex "+filename)
+os.remove(filename)
+os.remove(filenamebase+".aux")
+os.remove(filenamebase+".log")
 
-trials = 0
-while True:
-    trials = trials + 1
-    minmaxpos = 10000
-    for n in range(1, 1+participants):
-        filenamebase = "random%02d"%n
-        filename = filenamebase+".tex"
-        pos = 1
-        maxpos = 1
-        with open(filename,"w") as f:
-            print (latexstart, file=f)
-            for i in range(maxsteps):
-                if random.randint(1, 1+bias)==1:
-                    if pos > 1:
-                        pos = pos -1
-                        print ("\\item\\fbox{Go Back %d}\\\\"%pos, file=f)
-                    else:
-                        print ("\\item\\fbox{Stay %d}\\\\"%pos, file=f)
-                else:
-                    pos = pos + 1
-                    print ("\\item\\fbox{Go Forward %d}\\\\"%pos, file=f)
-                if pos > maxpos:
-                    maxpos = pos
-            print (latexfinish, file=f)
-            if minmaxpos > maxpos:
-                minmaxpos = maxpos
-        os.system("pdflatex "+filename)
-        os.remove(filename)
-        os.remove(filenamebase+".aux")
-        os.remove(filenamebase+".log")
-    if minmaxpos >= tiles:
-        print ("You can use up to", minmaxpos, "tiles (took",trials,"tries)")
-        break
-    else:
-        print ("************* NOT FINISHED IN 48 STEPS TRIALSING AGAIN ****************")
-        time.sleep(1)
-        
 
+print ("You can use up to", minmaxpos, "tiles.")        
